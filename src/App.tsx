09a7f726b2ac653995770a1c938f69a6ea43ad21
EU\tamosarn <arnoldas.tamosiunas@tieto.com>
2021-01-06 23:07:55 +0200

    React app init and libs added

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import './App.scss';
+import React, { Component } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = {
+    persons: []
+  }
+
+  componentDidMount() {
+    axios.get(`https://swapi.dev/api/people`)
+        .then(res => {
+          const persons = res.data.results;
+          this.setState({persons});
+        })
+  }
+
+  render() {
+    return (
+        <Router>
+          <div className='app__container'>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/">Home</Link>
+                </li>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path="/about">
+                <About/>
+              </Route>
+              <Route path="/users">
+                <Users users={this.state.persons}/>
+              </Route>
+              <Route path="/">
+                <Home/>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+    )
+  }
 }
 
-export default App;
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users(props) {
+  const users =
+      <>
+        {
+          props.users?.map(user => (
+              <>
+                <li>{user.name}</li>
+                <br/>
+              </>
+          ))
+        }
+      </>;
+
+  return props.users.length ? <ul>{users}</ul> : <h2>Loading...</h2>;
+}
